ff71dcd6e9a56d7b4e352f18a7824b59df95b8f6
Nukui <nukui.s.aa@gmail.com>
2015-12-26 11:29:49 +0900

    Add constraints of normalization and non-negative

--- bifac.py
+++ bifac.py
@@ -8,7 +8,7 @@ License: GNU General Public License Version 2
 import numpy as np
 import tensorflow as tf
 
-INFINITY = 10e+100
+INFINITY = 10e+1
 
 class BiFac(object):
     """class for BiFac algorithm
@@ -26,24 +26,25 @@ class BiFac(object):
         -------------------
     """
 
-    def __init__(self, edge_list, K):
+    def __init__(self, edge_list, weights, K, learning_rate=0.01):
             num_node = []
             for q in range(2):
                 nn = max(map(lambda x: x[q], edge_list)) + 1
                 num_node.append(nn)
 
             #id pairs of edge_list and weights
-            edge_list_ = [(e[0], e[1]) for e in edge_list]
-            weights = np.array([e[2] for e in edge_list]).astype(np.float32)
+            self.edge_list = edge_list
+            weights = np.array(weights, dtype=np.float32)
+            sum_weight = np.sum(weights)
 
-            self.X = X = tf.sparse_to_dense(
-                                    sparse_indices=edge_list_,
-                                    output_shape=num_node,
-                                    sparse_values=weights,
-                                    )
+            self.X = X = tf.SparseTensor(values=weights,
+                                                        indices=edge_list,
+                                                        shape=(num_node))
 
+            initializer_z = tf.random_normal_initializer(mean=sum_weight/K,
+                                                                            stddev=1.0)
             self.z = z = tf.get_variable(name="z", shape=[K],
-                                                    initializer=tf.random_uniform_initializer())
+                                                    initializer=initializer_z)
 
             self.U1 = U1 = tf.get_variable(name="U1", shape=[num_node[0], K],
                                                         initializer=tf.random_uniform_initializer())
@@ -51,30 +52,77 @@ class BiFac(object):
             self.U2 = U2 = tf.get_variable(name="U2", shape=[num_node[1], K],
                                                         initializer=tf.random_uniform_initializer())
 
-            #z_diag = tf.diag(z)
-
             U2_T = tf.transpose(U2)
-            U_multiple_ = [tf.matmul(
+            S = [z[k] * tf.matmul(
                                             tf.reshape(U1[:, k], shape=(num_node[0], 1)),
                                             tf.reshape(U2_T[k, :], shape=(1, num_node[1]))
                                             ) for k in range(K)]
             #(K * N1 * N2)-Tensor
-            U_multiple = tf.pack(U_multiple_)
-            self.Y = Y = tf.reduce_sum( z * U_multiple,
-                                        reduction_indices=[0])
+            self.S = S = tf.pack(S)
+            self.Y = Y = tf.reduce_sum(S, reduction_indices=[0])
+            self.y_values = y_values = tf.pack([Y[index] for index in edge_list])
 
-            KL_divergence = tf.reduce_sum(X * (tf.log(X) - tf.log(Y)) - X + Y)
+            self.x_values = x_values = X.values
+            sum_X = tf.reduce_sum(x_values)
+            sum_Y = tf.reduce_sum(Y)
 
-            self.loss = loss = KL_divergence
+            self.sum_MI = sum_MI = tf.reduce_sum(x_values * (tf.log(x_values / y_values)))
+            KL_divergence = sum_MI - sum_X + sum_Y
 
+            self.sum_u1_i = sum_u1_i = tf.reduce_sum(U1, reduction_indices=[0])
+            self.norm_u1 = normalize_U1 = tf.reduce_sum(tf.pow(
+                                         sum_u1_i - 1.0, 2))
+            self.sum_u1 = sum_u1 = tf.pow(tf.reduce_sum(U1 - 1.0), 2)
+            normalize_U2 = tf.reduce_sum(tf.pow(
+                                        tf.reduce_sum(U2, reduction_indices=[0]) - 1, 2
+                                        ))
+            constraint_z = tf.pow(tf.reduce_sum(z) - sum_weight, 2)
+            nonnegative_U1 = tf.reduce_sum(tf.abs(U1) - U1)
+            nonnegative_U2 = tf.reduce_sum(tf.abs(U2) - U2)
+
+            constraint = normalize_U1 + normalize_U2 + nonnegative_U1 + nonnegative_U2
+
+            self.loss = loss = KL_divergence + (nonnegative_U1 + nonnegative_U2) * INFINITY + \
+                                            (normalize_U1 + normalize_U2 + constraint_z)*2
+
+            optimizer = tf.train.AdagradOptimizer(learning_rate)
+            self.optimize = optimizer.minimize(loss)
+
+            u1_hist = tf.histogram_summary("U1", U1)
+            loss_summ = tf.scalar_summary("loss", loss)
+            self.merged = tf.merge_all_summaries()
+
+    def run(self, sess):
+        writer = tf.train.SummaryWriter("bifaclog", sess.graph_def)
+        tf.initialize_all_variables().run()
+        print(self.loss.eval())
+        print(self.U1.eval())
+        for iter_ in range(10000):
+            z, U1, U2, merged, _ = sess.run([self.z, self.U1, self.U2,
+                                                            self.merged, self.optimize])
+            if iter_ % 100 == 0:
+                writer.add_summary(merged, iter_)
+        print(self.loss.eval())
+        print(self.x_values.eval())
+        print(self.y_values.eval())
+
+        print(self.sum_u1_i.eval())
+        print(self.Y.eval())
+        return z, U1, U2
 
 if __name__ == '__main__':
-    edge_list = [(0,0,1), (0,1,1), (1,1,1),(2,1,2)]
+    edge_list = [(0,0), (0,1), (1,1),(1,0),(2,2),(2,3),(3,2),(3,3)]
+    weights = np.ones(len(edge_list))
     K = 2
-    bifac = BiFac(edge_list, K)
-    a = tf.ones([4,3,2])
-    z = tf.ones([2])
-    y = tf.ones([3])
-
+    bifac = BiFac(edge_list, weights, K)
     sess = tf.InteractiveSession()
-    tf.initialize_all_variables().run()
+    z, u1, u2 = bifac.run(sess)
+    print(z)
+    print(u1)
+    print(u2)
+    a = tf.ones([3, 4,2])
+    z = tf.constant([1., 2])
+    y = tf.ones([4])
+    #sess = tf.InteractiveSession()
+    #sp = tf.SparseTensor(values=y,indices=edge_list, shape=(4,2,3))
+    #tf.initialize_all_variables().run()
